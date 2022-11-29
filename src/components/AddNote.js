55acf46e497427980314df8a859accf836f4ae0e
vinnu9112 <111413210+vinnu9112@users.noreply.github.com>
2022-11-29 15:17:14 +0530

    third commit

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -5,7 +5,7 @@ const AddNote = () => {
     const context = useContext(NoteContext);
     const{addNote} = context;
 
-    const [note, setNote] = useState({title: "", description: "", tag:"default"});
+    const [note, setNote] = useState({title: "", description: "", tag:""});
     const handleClick = (e)=>{
         e.preventDefault();
         addNote(note.title, note.description, note.tag);
@@ -19,16 +19,16 @@ const AddNote = () => {
             <h1>Add Note</h1>
             <form className = "my-2">
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Title</label>
+                    <label htmlFor="title" className="form-label">Title</label>
                     <input type="text" className="form-control" id="title" name = "title" aria-describedby="emailHelp" onChange={onChange}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="description" className="form-label">Description</label>
                     <input type="text" className="form-control" id="description" name= "description" onChange={onChange}/>
                 </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                <div className="mb-3">
+                    <label htmlFor="tag" className="form-label">Tag</label>
+                    <input type="text" className="form-control" id="tag" name= "tag" onChange={onChange}/>
                 </div>
                 <button type="submit" className="btn btn-primary" onClick={handleClick}>Add Note</button>
             </form>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -24,21 +24,20 @@ const notesInitial = [
     setNotes(json);
     
   }
-  const addNote = async (title, description, tag)=>{
-    //API call
+  const addNote = async (title, description, tag) => {
+    // TODO: API Call
+    // API Call 
     const response = await fetch(`${host}/api/notes/addnote`, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'auth-token' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjM4Mzc4ZTIzMjQwYmVlNjk2MTY4NmJmIn0sImlhdCI6MTY2OTYxNTE1MX0.fSVRFEDKFVKbdbryBZqcxcwpTuXR4igrMKUwvSKdG7Y'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjM4Mzc4ZTIzMjQwYmVlNjk2MTY4NmJmIn0sImlhdCI6MTY2OTYxNTE1MX0.fSVRFEDKFVKbdbryBZqcxcwpTuXR4igrMKUwvSKdG7Y"
       },
-      body: JSON.stringify({title, description, tag}) 
+      body: JSON.stringify({title, description, tag})
     });
-    const json = await response.json(); 
-    console.log(json);
-    //Add a note on client side
-  console.log("Adding a note")
+     
+
+    console.log("Adding a new note")
     const note = {
       "_id": "63845be82ef9fac898de3a889",
       "user": "638378e23240bee6961686bf",
@@ -48,11 +47,24 @@ const notesInitial = [
       "date": "2022-11-28T06:57:44.298Z",
       "__v": 0
     }
-    setNotes(notes.concat(note));
+    setNotes(notes.concat(note))
   }
   
-  //Delete a note
-  const deleteNote = (id)=>{
+  const deleteNote = async (id)=>{
+    //API call
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjM4Mzc4ZTIzMjQwYmVlNjk2MTY4NmJmIn0sImlhdCI6MTY2OTYxNTE1MX0.fSVRFEDKFVKbdbryBZqcxcwpTuXR4igrMKUwvSKdG7Y'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      }
+    });
+    const json = await response.json(); 
+    console.log(json);
+
+
+    //Delete a note
     console.log("deleting the note with id"+ id);
     const newsNotes = notes.filter((note)=>{
       return note._id !== id;
